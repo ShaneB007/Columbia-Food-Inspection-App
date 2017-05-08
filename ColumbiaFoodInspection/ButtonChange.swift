12277db79f786850f498ee375ed9d0e5dec46166
Kaylyn Sneathen <kss9tf@mail.missouri.edu>
2017-05-08 13:31:19 -0500

    Added pictures, set up status for pictures

---
+++ ColumbiaFoodInspection/ButtonChange.swift
@@ -0,0 +1,52 @@
+//
+//  ButtonChange.swift
+//  ColumbiaFoodInspection
+//
+//  Created by Kaylyn Sneathen on 5/5/17.
+//  Copyright © 2017 Columbia Food Inspection Group. All rights reserved.
+//
+
+import UIKit
+
+class ButtonChange: UIViewController {
+
+    
+    @IBOutlet weak var favoriteButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        // Do any additional setup after loading the view.
+        //Create New Button
+       
+        //set image for Button
+        //let starEmpty = UIImage(named: #imageLiteral(resourceName: "Star-50")) as UIImage!
+        //let starFull = UIImage(named: #imageLiteral(resourceName: "Star Filled-50")) as UIImage!
+        //self.favoriteButton.setImage(starEmpty, for: UIControlState.normal)
+        //self.favoriteButton.setImage(starFull, for: UIControlState.selected)
+        
+        favoriteButton.setImage(UIImage?( #imageLiteral(resourceName: "Star-50")), for: .normal)
+        favoriteButton.setImage(UIImage?(#imageLiteral(resourceName: "Star Filled-50")), for: .selected)
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
