339b271fc8b0edb63b2a64bd2c47ee7bc264e284
Topguny <lukasporter@sbcglobal.net>
2017-05-10 10:15:51 -0500

    HOLDER

--- ColumbiaFoodInspection/AboutViewController.swift
+++ ColumbiaFoodInspection/AboutViewController.swift
@@ -16,6 +16,7 @@ class AboutViewController: UIViewController{
         
         infoPageTitle.backgroundColor = UIColor.init(red: 0.09, green: 0.38, blue: 0.62, alpha: 1.0)
         info.backgroundColor = UIColor.init(red: 0.46, green: 0.45, blue: 0.45, alpha: 1.0)
+        view.backgroundColor = UIColor.gray
         
         infoPageTitle.text = "Information"
         info.text = "Violation Color Description \n*Red = 5 or more critical violations, total violations > 20 \n*Orange = 2 -5 critical, non critical > 10 \n*Yellow = 1-2 critical, 5-10 non critical \n*Green  = 0-1 critical, non-critical < 5 \n\nGroup: \nShane Bishop, Colin Iglehart, Lukas Heading, Matthew Pokoik, Trevor Beshear, Kaylyn Sneathen"
